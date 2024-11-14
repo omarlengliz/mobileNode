5fc60078dad686fe7340b2352e948666191a89b3
omar lengliz <omarlengliz12@gmail.com>
2024-11-14 09:13:07 +0100

    config

--- index.js
+++ index.js
@@ -11,19 +11,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 connectDB()
 const admin = require('firebase-admin');
+const serviceAccount = require('./service.json');
 admin.initializeApp({
-  credential: admin.credential.cert({
-    type: process.env.FIREBASE_TYPE,
-    project_id: process.env.FIREBASE_PROJECT_ID,
-    private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
-    private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
-    client_email: process.env.FIREBASE_CLIENT_EMAIL,
-    client_id: process.env.FIREBASE_CLIENT_ID,
-    auth_uri: process.env.FIREBASE_AUTH_URI,
-    token_uri: process.env.FIREBASE_TOKEN_URI,
-    auth_provider_x509_cert_url: process.env.FIREBASE_AUTH_PROVIDER_CERT_URL,
-    client_x509_cert_url: process.env.FIREBASE_CLIENT_CERT_URL,
-  })
+  credential: admin.credential.cert(serviceAccount)
 });
 app.use('/uploads', express.static('uploads'));
 app.use(cors(
